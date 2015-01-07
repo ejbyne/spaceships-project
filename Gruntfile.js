eec8255fda4d6ff0f83fea573e26de15ae9657d8
Claudia <claudia.donovan89@gmail.com>
2015-01-07 12:53:17 +0000

    setting up

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+module.exports = function(grunt) {
+
+ grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    mocha_casperjs: {
+      options: {
+      },
+      files: {
+        src: ['test/**/*']
+      }
+    }
+  })
+  grunt.loadNpmTasks('grunt-mocha-casperjs');
+  grunt.loadNpmTasks('grunt-jasmine');
+
+  grunt.registerTask('default', ['jasmine']);
+};
